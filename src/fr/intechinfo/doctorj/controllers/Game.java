59c92a1d084fc815e17f4fb917568519a73341b3
Alexandre Da Silva <dev.dasilva.alexandre@gmail.com>
2014-06-04 14:31:45 +0200

    Fix some bugs after merge develop/testValidator

--- src/fr/intechinfo/doctorj/controllers/Game.java
+++ src/fr/intechinfo/doctorj/controllers/Game.java
@@ -1,6 +1,6 @@
 package fr.intechinfo.doctorj.controllers;
 
-import fr.intechinfo.doctorj.model.SyntaxValidator;
+import fr.intechinfo.doctorj.model.validators.SyntaxValidator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
