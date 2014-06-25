edd161e5b470d9ff9c4f18827ad4d620bc0b84ab
Adeline MARTIN <amartin@intechinfo.fr>
2014-06-25 11:29:27 +0200

    Colorsyntax + bug accolade

--- src/fr/intechinfo/doctorj/controllers/Game.java
+++ src/fr/intechinfo/doctorj/controllers/Game.java
@@ -4,14 +4,21 @@ import fr.intechinfo.doctorj.model.ApplicationContext;
 import fr.intechinfo.doctorj.model.validators.SyntaxValidator;
 import fr.intechinfo.doctorj.model.validators.TestValidator;
 import fr.intechinfo.doctorj.model.validators.ValidatorMessage;
+import javafx.embed.swing.SwingNode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.apache.commons.io.FileUtils;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,6 +30,7 @@ public class Game extends AbstractController implements Initializable {
     @FXML private TextArea codeArea;
     @FXML private TextArea errorArea;
     @FXML private Button btnHome;
+    @FXML private VBox vBoxCode;
 
     public Game(Stage mainWindow, String viewName) {
         super(mainWindow, viewName);
@@ -30,7 +38,33 @@ public class Game extends AbstractController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        SwingNode swingNode = new SwingNode();
+        vBoxCode.getChildren().add(swingNode);
+
+        createSwingContent(swingNode);
+        
          
+    }
+
+    private void createSwingContent(final SwingNode swingNode) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                JPanel cp = new JPanel(new BorderLayout());
+
+
+
+                RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+                textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+                textArea.setCodeFoldingEnabled(true);
+                RTextScrollPane sp = new RTextScrollPane(textArea);
+                cp.add(sp);
+
+                swingNode.setContent(cp);
+
+            }
+        });
     }
 
     @FXML protected void onClickBtnHome(ActionEvent event) {
