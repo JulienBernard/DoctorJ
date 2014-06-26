744989870d0987c3f981422222c4fd6c630b9a4a
Alexandre Da Silva <dev.dasilva.alexandre@gmail.com>
2014-06-26 23:06:46 +0200

    Changed Java Compiler from JDK to Eclipse Compiler

--- src/fr/intechinfo/doctorj/model/validators/SyntaxValidator.java
+++ src/fr/intechinfo/doctorj/model/validators/SyntaxValidator.java
@@ -1,5 +1,7 @@
 package fr.intechinfo.doctorj.model.validators;
 
+import org.eclipse.jdt.internal.compiler.tool.EclipseCompiler;
+
 import javax.tools.*;
 import java.util.*;
 
@@ -8,8 +10,7 @@ import java.util.*;
  */
 public class SyntaxValidator {
     public static ValidatorMessage check(String file) {
-
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        JavaCompiler compiler = new EclipseCompiler();
 
         StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
         Iterable<? extends JavaFileObject> compilationUnits =
@@ -21,11 +22,20 @@ public class SyntaxValidator {
         List<ValidatorMessageElement> elements = new ArrayList<>();
 
         for (Diagnostic diagnostic : diagnostics.getDiagnostics()) {
+            ValidatorConstants vc;
+
+            if(diagnostic.getKind() == Diagnostic.Kind.WARNING) {
+                vc = ValidatorConstants.WARNING;
+            } else if (diagnostic.getKind() == Diagnostic.Kind.ERROR) {
+                vc = ValidatorConstants.ERROR;
+            } else {
+                vc = ValidatorConstants.BLANK;
+            }
+
             elements.add(
                     new ValidatorMessageElement(
                             diagnostic.getKind() + ":\t Line [" + diagnostic.getLineNumber() + "] \t Position [" + diagnostic.getPosition() +
-                                    "] \t" + diagnostic.getMessage(Locale.FRENCH) + "\n",
-                            ValidatorConstants.ERROR
+                                    "] \t" + diagnostic.getMessage(Locale.FRENCH) + "\n", vc
                     )
             );
         }
