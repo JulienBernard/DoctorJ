a01e0e0b8806676bb3338521070de20c05889d6e
JulienBernard <jbernard@intechinfo.fr>
2014-06-27 17:07:15 +0200

    We can now load a story and navigate into it when the code as approved.

--- src/fr/intechinfo/doctorj/controllers/Game.java
+++ src/fr/intechinfo/doctorj/controllers/Game.java
@@ -1,6 +1,8 @@
 package fr.intechinfo.doctorj.controllers;
 
 import fr.intechinfo.doctorj.DoctorJ;
+import fr.intechinfo.doctorj.model.Chapter;
+import fr.intechinfo.doctorj.model.Step;
 import fr.intechinfo.doctorj.model.Storyline;
 import fr.intechinfo.doctorj.model.validators.SyntaxValidator;
 import fr.intechinfo.doctorj.model.validators.TestValidator;
@@ -8,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.media.Media;
 import javafx.stage.Stage;
@@ -28,6 +31,10 @@ public class Game extends AbstractController implements Initializable {
     @FXML private TextArea errorArea;
     @FXML private Button btnHome;
     @FXML private MediaView background;
+    @FXML private Label lblHelp;
+    @FXML private Label lblHint;
+    @FXML private Label lblChapterName;
+    @FXML private Label lblChapterDescription;
 
     public static final String VID_URL = "file:./src/fr/intechinfo/doctorj/assets/test.mp4";
 
@@ -41,15 +48,13 @@ public class Game extends AbstractController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        /*
-        Media clip = new Media(VID_URL);
-        MediaPlayer player = new MediaPlayer(clip);
-        player.setAutoPlay(true);
-        background.setMediaPlayer(player);
-        */
+        // Data select for the first chapter / first step
+        getStory().setCurrentChapter(0);
+        int currentChapter = getStory().getCurrentChapter();
+        getStory().getChapters().get(currentChapter).setCurrentStep(0);
+        int currentStep = getStory().getChapters().get(currentChapter).getCurrentStep();
 
-        // Example:
-        System.out.println(getStory().getName());
+        updateData();
     }
 
     @FXML protected void onClickBtnHome(ActionEvent event) {
@@ -74,8 +79,39 @@ public class Game extends AbstractController implements Initializable {
 
         if(errors.length() < 10 && codeArea.getText().length() > 6) {
             errors += TestValidator.check("story1", "Step1").getMessage();
-
             errorArea.setText(errors);
         }
+
+        if( errorList.isEmpty() ) {
+            errors += "Win";
+            errorArea.setText(errors);
+            setCurrentStepPlus();
+            updateData();
+        }
+    }
+
+    private Chapter getCurrentChapter() {
+        return getStory().getChapters().get(getStory().getCurrentChapter());
+    }
+
+    private void setCurrentChapterPlus() {
+        getStory().setCurrentChapter(getStory().getCurrentChapter() + 1);
+    }
+
+    private Step getCurrentStep() {
+        Chapter chap = getCurrentChapter();
+        return getCurrentChapter().getSteps().get(chap.getCurrentStep());
+    }
+
+    private void setCurrentStepPlus() {
+        Chapter chap = getCurrentChapter();
+        chap.setCurrentStep(chap.getCurrentStep() + 1);
+    }
+
+    private void updateData() {
+        this.lblChapterName.setText( getCurrentChapter().getTitle() );
+        this.lblChapterDescription.setText( getCurrentChapter().getDescription() );
+        this.lblHelp.setText( getCurrentStep().getHelp() );
+        this.lblHint.setText( getCurrentStep().getHint() );
     }
 }
